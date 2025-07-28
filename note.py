9788cc68ce29b1fd5545238f7a5bccf4506b4807
KimSiJin <kimsijin33@gmail.com>
2025-07-28 23:04:56 +0900

    docs: coding on p75

--- note.py
+++ note.py
@@ -8,5 +8,23 @@ x**2 + 6*x + 5
 >>> factor(x**2 + 6*x + 5)
 (x + 1)*(x + 5)
 
+#250728 p75
+>>> import math
+>>> pow = math.pow(2, 5)
+>>> print("pow 결과" ".format(pow))
+  File "<stdin>", line 1
+    print("pow 결과" ".format(pow))
+                                 ^
+SyntaxError: EOL while scanning string literal
+>>> print("pow 결과: {}".format(pow))
+pow 결과: 32.0
+>>> sqrt = math.sqrt(2)
+>>> print(f"sqrt 결과: {sqrt}")
+sqrt 결과: 1.4142135623730951
+>>> exp = math.exp(2)
+>>> print(f"exp 결과: {exp}")
+exp 결과: 7.38905609893065
+
+
 
 
